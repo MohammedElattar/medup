bcf58d91931398d217b9e9c3822bf26cda453382
Mohamed Attar <mohammedattar0100020@gmail.com>
2025-03-24 16:08:48 +0200

    fix: fix subscription migration

--- Modules/Subscription/Database/Migrations/2025_01_29_115522_create_subscriptions_table.php
+++ Modules/Subscription/Database/Migrations/2025_01_29_115522_create_subscriptions_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('expert_id')->constrained()->cascadeOnDelete();
             $table->timestamp('starts_at');
             $table->timestamp('ends_at');
             $table->boolean('paid')->default(false);
