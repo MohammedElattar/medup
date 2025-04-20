8076d5bd0cd98d9993fb1680b1a07483bfb5e28e
Mohamed Attar <mohammedattar0100020@gmail.com>
2025-04-20 20:16:06 +0200

    test: test chat

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,9 @@ Route::group(['prefix' => 'api/select_menu'], function(){
     Route::get('specialities', [SelectMenuController::class, 'specialities']);
     Route::get('colleges_with_specialities', [SelectMenuController::class, 'collegesWithSpecialities']);
 });
+
+Route::get('test_event', function(){
+    $conversation = \Modules\Chat\Models\Conversation::query()->first();
+    $message = \Modules\Chat\Models\ConversationMessage::query()->first();
+    \Modules\Chat\Events\ConversationUpdatedEvent::dispatch($conversation->id, $message);
+});
