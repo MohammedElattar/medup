189981e2a1f1468e4a1bd7cdc1486c1496f05e45
Mohamed Attar <mohammedattar0100020@gmail.com>
2025-06-18 21:05:38 +0300

    fix(skills): filters

--- Modules/Skill/Services/PublicSkillService.php
+++ Modules/Skill/Services/PublicSkillService.php
@@ -8,10 +8,13 @@ class PublicSkillService
 {
     public function index()
     {
+        $specialities = array_filter(explode(',', request()->input('specialities', '')) ?: []);
+
         return Skill::query()
             ->latest()
             ->with('icon')
             ->withCount('experts')
+            ->when(!empty($specialities), fn($q) => $q->whereHas('specialities', fn($q) => $q->whereIn('specialities.id', $specialities)))
             ->searchable()
             ->paginatedCollection();
     }
